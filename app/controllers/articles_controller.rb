c239518f454b30a3c8c99c4120eeb085c5f3dd8d
Ali Akbar <aliqadri100@gmail.com>
2021-02-10 20:54:34 +0500

    Removed redundancy from controller, prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,14 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
     def index
         @articles=Article.all
     end
+
     def new
         @article=Article.new
     end
 
     def edit
-        @article=Article.find(params[:id])
     end
 
     def create
@@ -22,7 +23,6 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article=Article.find(params[:id])
         if @article.update(article_params)
             flash[:notice]="Article updated successfully!"
             redirect_to article_path(@article)
@@ -32,17 +32,18 @@ class ArticlesController < ApplicationController
     end
 
     def show
-        @article=Article.find(params[:id])
     end
 
     def destroy
-        @article=Article.find(params[:id])
         @article.destroy
         flash[:notice]="Article deleted successfully!"
         redirect_to articles_path
     end
 
     private
+        def set_article
+            @article=Article.find(params[:id])
+        end
         def article_params
             params.require(:article).permit(:title,:description)
         end
