175aeee6b82f859fbc3fad09540ff51b996ad895
Ali Akbar <aliqadri100@gmail.com>
2021-02-10 20:47:27 +0500

    added partials and delete functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,13 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles=Article.all
+    end
     def new
         @article=Article.new
+    end
+
+    def edit
+        @article=Article.find(params[:id])
     end
 
     def create
@@ -14,8 +21,25 @@ class ArticlesController < ApplicationController
         end       
     end
 
+    def update
+        @article=Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice]="Article updated successfully!"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end 
+    end
+
     def show
         @article=Article.find(params[:id])
+    end
+
+    def destroy
+        @article=Article.find(params[:id])
+        @article.destroy
+        flash[:notice]="Article deleted successfully!"
+        redirect_to articles_path
     end
 
     private
