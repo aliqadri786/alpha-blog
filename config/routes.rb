a365c6d02d36c855598ba206e06a8a2c803d7550
Ali Akbar <aliqadri100@gmail.com>
2021-02-17 10:02:35 +0500

    added signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'pages/home', to: 'pages#home'
-  get 'pages/about', to: 'pages#about'
+  get 'pages/about', to: 'pages#about'  
 
   resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
