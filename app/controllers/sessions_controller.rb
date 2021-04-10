3158788eab15c41e395282015dbc2543fa7b1b0f
Ali Akbar <aliqadri100@gmail.com>
2021-04-10 17:00:19 +0500

    fixes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,17 +10,17 @@ class SessionsController < ApplicationController
         
         if user && user.authenticate(params[:session][:password])
         
-        session[:user_id] = user.id
-        
-        flash[:success] = "You have successfully logged in"
-        
-        redirect_to user_path(user)
+            session[:user_id] = user.id
+            
+            flash[:success] = "You have successfully logged in"
+            
+            redirect_to user_path(user)
         
         else
         
-        flash.now[:danger] = "There was something wrong with your login information"
-        
-        render 'new'
+            flash.now[:danger] = "There was something wrong with your login information"
+            
+            render 'new'
         
         end
         
